a9aa3082973d9fb4632cf08fb93b222cc5e9af03
pixiepucker <autumnoffic@gmail.com>
2022-05-02 19:29:21 -0500

    finished CRUD routes for posts

--- models/index.js
+++ models/index.js
@@ -1,4 +1,15 @@
+//requirements
 const User = require('./User');
 const Post = require('./Post');
+
+//create associations of one-to-many rel
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+//reverse association
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
 
 module.exports = { User, Post };
---
+++ routes/api/post-routes.js
@@ -0,0 +1,112 @@
+//requirements
+const router = require('express').Router();
+const { Post, User } = require('../../models');
+
+//get all posts
+router.get('/', (req,res) => {
+    console.log('==================');
+    Post.findAll({
+        //query config
+        attributes: ['id', 'post_url', 'title', 'created_at'],
+        order: [['created_at', 'DESC']],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+//get one post by id
+router.get('/:id', (req,res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'post_url', 'title', 'created_at'],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post with this id exists!'});
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+//create post
+router.post('/', (req,res) => {
+    Post.create({
+        title: req.body.title,
+        post_url: req.body.post_url,
+        user_id: req.body.user_id
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+//update title
+router.put('/:id', (req,res) => {
+    Post.update(
+        {
+            title: req.body.title
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+    )
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post with that id exists!'});
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+//delete an entry
+router.delete('/:id', (req,res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post with that id exists!'});
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
